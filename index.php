35ea1adb1ebe4a5c4915a3e76f5a19a0018f6d1a
developer-siddhisoftwarecompany <dev.siddhisoftwarecompany@gmail.com>
2025-11-28 08:58:54 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+
+$userDatabase = [
+    "101" => [
+        "username" => "keshav_12",
+        "device_id" => "ABCD1234XYZ",
+        "login_time" => "10:45 AM",
+        "login_date" => "2025-11-28",
+        "country_code" => "IN",
+        "user" => [
+            "age" => 23,
+            "name" => "Keshav Agarwal",
+            "photo" => "https://example.com/pic.jpg",
+            "phone" => "+91 9876543210",
+            "address" => "New Delhi, India"
+        ]
+    ]
+];
+
+$id = $_GET["id"] ?? null;
+
+if (!$id) {
+    echo json_encode(["error" => "Missing id parameter"]);
+    exit;
+}
+
+if (!isset($userDatabase[$id])) {
+    echo json_encode(["error" => "User not found"]);
+    exit;
+}
+
+$data = $userDatabase[$id];
+
+$response = [
+    "loginDetails" => [
+        "username" => $data["username"],
+        "deviceId" => $data["device_id"],
+        "loginTime" => $data["login_time"],
+        "loginDate" => $data["login_date"],
+        "countryCode" => $data["country_code"]
+    ],
+    "userDetails" => [
+        "age" => $data["user"]["age"],
+        "name" => $data["user"]["name"],
+        "photo" => $data["user"]["photo"],
+        "phone" => $data["user"]["phone"],
+        "address" => $data["user"]["address"]
+    ],
+    "systemDetails" => [
+        "currentDate" => date("Y-m-d"),
+        "currentSystem" => php_uname()
+    ]
+];
+
+echo json_encode($response, JSON_PRETTY_PRINT);
+?>
